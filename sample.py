dbf45c72e0da313ff5fb61cf53bbec2af95eb774
Kazu Komoto <kazuhiro.komoto@students.makeschool.com>
2015-09-28 14:33:57 -0700

    Made sample.py which detect the probability of the words in textfile

--- sample.py
+++ sample.py
@@ -8,7 +8,22 @@ def sample(filename):
     random_index = random.randint(0, len(list_of_words) - 1)
     # print(list_of_words[random_index])
     return(list_of_words[random_index])
-    
+
+def samle_probability(filename):
+    text_file = open(filename, "r")
+    list_of_words = text_file.read().split()
+
+    words_count = {}
+    for each_word in list_of_words:
+        words_count[each_word] = words_count.get(each_word, 0) + 1
+
+    words_probability = {}
+    for each_word in words_count:
+        words_probability[each_word] = words_count[each_word] / len(list_of_words)
+
+    print(words_probability)
+
 if __name__ == '__main__':
     filename = sys.argv[1]
     print(sample(filename))
+    print(samle_probability(filename))
